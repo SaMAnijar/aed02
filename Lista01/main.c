20a296ca0541be65af24a98750680aade73b8273
Samuel B Anijar <128177565+SaMAnijar@users.noreply.github.com>
2023-04-21 21:20:07 -0400

    Update main.c

--- Lista01/main.c
+++ Lista01/main.c
@@ -27,6 +27,29 @@
 
 int main(){
     questao01();
+    //questao02();
+    //questao03();
+    //questao04();
+    //questao05();
+    //questao06();
+    //questao07();
+    //questao08();
+    //questao09();
+    //questao10();
+    //questao11();
+    //questao12();
+    //questao13();
+    //questao14();
+    //questao15();
+    //questao16();
+    //questao17();
+    //questao18();
+    //questao19();
+    //questao20();
+    //questao21();
+    //questao22();
+    //questao23();
+    //questao24();
 
     return 0;
 }
